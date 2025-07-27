7ecd9897b0503239d990dac9cbab8144af01a39e
Bruno Panizzi <brunopanizziq@gmail.com>
2025-07-27 20:47:00 -0300

    add routers to the app

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { migrate } from "./db/migrate.js";
 import { get_db } from "./db/index.js";
 
 import { createUsuarioRouter } from "./controllers/usuario.controller.js";
+import { createAlunoRouter } from "./controllers/aluno.controller.js";
+import { createProfessorRouter } from "./controllers/professor.controller.js";
+import { createSecretariaRouter } from "./controllers/secretaria.controller.js";
 
 config()
 
@@ -24,6 +27,9 @@ app.get("/", (req, res) => {
 })
 
 app.use('/usuarios', createUsuarioRouter(db));
+app.use('/alunos', createAlunoRouter(db));
+app.use('/professores', createProfessorRouter(db));
+app.use('/secretarias', createSecretariaRouter(db));
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
